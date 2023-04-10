326aafc7600e725b3dd0a5fe5f65fe9f1d753b67
davyd <davydow.val@yandex.ru>
2023-04-10 15:53:08 +0700

    tests for item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,31 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def new_item():
+    yield Item("item1", 100.0, 5)
+
+
+def test_calculate_total_price(new_item):
+    assert new_item.calculate_total_price() == 500.0
+
+
+def test_apply_discount(new_item):
+    new_item.pay_rate = 0.9
+    new_item.apply_discount()
+    assert new_item.price == 90.0
+
+
+def test_name(new_item):
+    assert new_item.name == "item1"
+
+
+def test_price(new_item):
+    assert new_item.price == 100.0
+
+
+def test_quantity(new_item):
+    assert new_item.quantity == 5
